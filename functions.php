16ca1fe22737482020b73338bf9ef439c9c3bc90
Jure Cuhalev <jure@cuhalev.com>
2016-10-01 15:35:52 +0200

    One more row on homepage

--- functions.php
+++ functions.php
@@ -53,7 +53,7 @@ if( function_exists('acf_add_options_page') ) {
 
 function get_homepage_posts() {
     $args = array('post_type' => 'post',
-                  'posts_per_page' => 8);
+                  'posts_per_page' => 12);
     return new WP_Query($args);
 }
 
