4af95447075d629da78679bb4806a12871ff52aa
Jure Cuhalev <jure@cuhalev.com>
2016-10-13 21:44:15 +0200

    Webinars archives

--- inc/actions.php
+++ inc/actions.php
@@ -3,7 +3,18 @@
 function cccoer_filter_mainquery( $query ) {
     if ( $query->is_archive() && $query->is_main_query() && is_post_type_archive('webinar')) {
         $query->set('posts_per_page', 16);
+        $query->set('tax_query', array(
+            array(
+                'taxonomy' => 'webinar_category',
+                'field' => 'slug',
+                'terms' => 'oer-degree',
+                'operator' => 'NOT IN'
+            )
+        ));
+    } elseif ( is_tax('webinar_category') ) {
+        $query->set('posts_per_page', 16);
     }
+    
     return $query;
 }
 
