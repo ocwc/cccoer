ffecc78d060adc87f886e253713f2bc4c9f1c730
Jure Cuhalev <jure@cuhalev.com>
2016-12-12 12:50:56 +0100

    Bump safari modernizr

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('sass-devel', function() {
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
+      browsers: ['last 2 versions', 'ie >= 9', 'safari >= 8']
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('../css'));
