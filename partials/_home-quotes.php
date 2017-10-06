6653dff6ed39fa908e0d4a68251b7d5b73ea07cd
Jure Cuhalev <jure@cuhalev.com>
2017-10-06 18:30:12 +0200

    Move paragraph

--- partials/_home-quotes.php
+++ partials/_home-quotes.php
@@ -6,11 +6,6 @@
                      alt="CCCOER 10 Year Anniversary"/>
             </div>
             <div class="cell expanded medium-10">
-                <p class="intro">CCCOER is a growing consortium of community and technical colleges committed to
-                    expanding access to education and increasing student success through adoption of open educational
-                    policy, practices, and resources. We are celebrating our 10th Anniversary in 2017-18 with
-                    retrospectives from the amazing visionaries, leaders, and members who have contributed to building
-                    our community.</p>
                 <h4>10-Year Anniversary Retrospectives</h4>
 
                 <div class="grid-x grid-margin-x">
