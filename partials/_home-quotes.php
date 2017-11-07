9965eefef752644df988dfdd1b8834603150d64d
Jure Cuhalev <jure@cuhalev.com>
2017-11-07 09:58:08 +0100

    Randomise quotes on homepage

--- partials/_home-quotes.php
+++ partials/_home-quotes.php
@@ -8,10 +8,24 @@
             <div class="cell expanded medium-10">
                 <h4>10-Year Anniversary Retrospectives</h4>
 
+
+
                 <div class="grid-x grid-margin-x">
                     <?php $quote_posts = new WP_Query( array(
                         'post_type' => 'retrospective',
-                        'posts_per_page' => 3
+                        'posts_per_page' => 1,
+                    ) ); ?>
+                    <?php while ( $quote_posts->have_posts() ) : $quote_posts->the_post(); ?>
+                        <?php $post_id = $post->ID; ?>
+                        <?php get_template_part( 'partials/_card', 'quote' ); ?>
+                    <?php endwhile; ?>
+
+
+                    <?php $quote_posts = new WP_Query( array(
+                        'post_type' => 'retrospective',
+                        'posts_per_page' => 2,
+                        'post__not_in' => array($post_id),
+                        'orderby' => 'rand'
                     ) ); ?>
                     <?php while ( $quote_posts->have_posts() ) : $quote_posts->the_post(); ?>
                         <?php get_template_part( 'partials/_card', 'quote' ); ?>
