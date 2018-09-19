d83603c85ce9597e148d505415787b34ad673053
Jure Cuhalev <jure@cuhalev.com>
2018-09-19 18:54:09 +0200

    Change how top boxbar behaves

--- partials/_home-boxbar.php
+++ partials/_home-boxbar.php
@@ -1,20 +1,22 @@
 <div class="boxbar-container">
-    <div class="grid-container">
+    <div class="">
         <?php if ( have_rows( 'home_boxbar', 'option' ) ) : ?>
             <?php $c = 1; ?>
 
             <div class="grid-x grid-padding-x">
                 <?php while ( have_rows( 'home_boxbar', 'option' ) ) : the_row(); ?>
-                    <div class="medium-6 cell clearfix boxbar boxbar-<?= $c; ?> grid-y grid-padding-y align-justify">
-                        <div class="expand cell">
-                            <h4><?php the_sub_field( 'title' ); ?></h4>
-                            <?php the_sub_field( 'text' ); ?>
-                        </div>
+                    <div class="medium-6 cell clearfix grid-x align-center items-justify">
+                        <div class="boxbar boxbar-<?= $c; ?> grid-y grid-padding-y align-justify">
+                            <div class="expand cell">
+                                <h4><?php the_sub_field( 'title' ); ?></h4>
+                                <?php the_sub_field( 'text' ); ?>
+                            </div>
 
-                        <div class="shrink cell">
-                            <a href="<?php the_sub_field( 'link' ); ?>"
-                               class="button <?= $c === 1 ? '' : 'green' ?>"
-                            ><?php the_sub_field( 'button_text' ); ?></a>
+                            <div class="shrink cell">
+                                <a href="<?php the_sub_field( 'link' ); ?>"
+                                   class="button <?= $c === 1 ? '' : 'green' ?>"
+                                ><?php the_sub_field( 'button_text' ); ?></a>
+                            </div>
                         </div>
                     </div>
                     <?php $c ++; ?>
