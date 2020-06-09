07b777aa850a66f55774b01147d281a71bda0266
Jure Cuhalev <jure@cuhalev.com>
2020-06-09 16:39:38 +0200

    Fix awards link

--- partials/_global-banner.php
+++ partials/_global-banner.php
@@ -4,12 +4,11 @@
             <div class="cell grow">
                 <a class="global-header__item   " href="https://www.oeglobal.org">OE Global</a><a
                     class="global-header__item " href="https://www.oelatam.org">OE LATAM</a><a
-                    class="global-header__item active" href="https://www.cccoer.org">CCCOER</a><a class="global-header__item"
-                                                                                           href="https://conference.oeconsortium.org">OE
+                    class="global-header__item active" href="https://www.cccoer.org">CCCOER</a><a
+                    class="global-header__item"
+                    href="https://conference.oeconsortium.org">OE
                     Global Conference</a><a class="global-header__item" href="https://www.openeducationweek.org">OE
-                    Week</a><a class="global-header__item"
-                               href="https://www.oeconsortium.org/projects/open-education-awards-for-excellence/2019-winners-of-oe-awards/">OE
-                    Awards</a>
+                    Week</a><a class="global-header__item" href="https://awards.oeglobal.org">OE Awards</a>
             </div>
         </div>
     </div>
