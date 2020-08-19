4660d4126aa9495546520f65ea500c54cc6bc138
Jure Cuhalev <jure@cuhalev.com>
2020-08-19 12:53:05 +0200

    Implements EDI custom type

--- inc/post_types.php
+++ inc/post_types.php
@@ -215,57 +215,27 @@ function cc_post_type_Retrospective() {
 add_action( 'init', 'cc_post_type_retrospective', 0 );
 
 
-function cc_post_type_student_story() {
+add_action( 'init', function() {
+    register_extended_post_type( 'studentstory', [
+        'taxonomies'      => array( 'post_tag' ),
+        'capability_type' => 'page',
+        'menu_icon'       => 'dashicons-welcome-learn-more',
+        'menu_position'   => 5,
+        'show_in_rest'    => true,
+        'rest_base'       => 'studentstory',
+    ], [
+        'singular' => 'Student story',
+        'plural'   => 'Student stories'
+    ] );
 
-    $labels = array(
-        'name'                  => 'Student story',
-        'singular_name'         => 'Student story',
-        'menu_name'             => 'Student stories',
-        'name_admin_bar'        => 'Student stories',
-        'archives'              => 'Student story Archives',
-        'parent_item_colon'     => 'Parent Item:',
-        'all_items'             => 'All Items',
-        'add_new_item'          => 'Add New Item',
-        'add_new'               => 'Add New Student story',
-        'new_item'              => 'New Student story',
-        'edit_item'             => 'Edit Student story',
-        'update_item'           => 'Update Student story',
-        'view_item'             => 'View Student story',
-        'search_items'          => 'Search Item',
-        'not_found'             => 'Not found',
-        'not_found_in_trash'    => 'Not found in Trash',
-        'featured_image'        => 'Featured Image',
-        'set_featured_image'    => 'Set featured image',
-        'remove_featured_image' => 'Remove featured image',
-        'use_featured_image'    => 'Use as featured image',
-        'insert_into_item'      => 'Insert into item',
-        'uploaded_to_this_item' => 'Uploaded to this item',
-        'items_list'            => 'Items list',
-        'items_list_navigation' => 'Items list navigation',
-        'filter_items_list'     => 'Filter items list',
-    );
-    $args   = array(
-        'label'               => 'Student story',
-        'description'         => 'Student stories',
-        'labels'              => $labels,
-        'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions' ),
-        'taxonomies'          => array( 'post_tag' ),
-        'hierarchical'        => false,
-        'public'              => true,
-        'show_ui'             => true,
-        'show_in_menu'        => true,
-        'menu_position'       => 5,
-        'show_in_admin_bar'   => true,
-        'show_in_nav_menus'   => true,
-        'can_export'          => true,
-        'has_archive'         => true,
-        'exclude_from_search' => false,
-        'publicly_queryable'  => true,
-        'capability_type'     => 'page',
-        'menu_icon'           => 'dashicons-welcome-learn-more'
-    );
-    register_post_type( 'studentstory', $args );
-
-}
-
-add_action( 'init', 'cc_post_type_student_story', 0 );
+    register_extended_post_type( 'edi', [
+        'taxonomies'      => array( 'post_tag' ),
+        'capability_type' => 'page',
+        'menu_icon'       => 'dashicons-businesswoman',
+        'show_in_rest'    => true,
+        'rest_base'       => 'edi',
+    ], [
+        'singular' => 'EDI post',
+        'plural'   => 'EDI posts'
+    ] );
+} );
